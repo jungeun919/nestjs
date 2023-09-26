5367aa0fc48b9883c0c1ec343295f258533447e1
jungeun919 <renee8369@likelion.org>
2023-09-26 12:31:59 +0900

    get/delete/update board from id

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { BoardsService } from "./boards.service";
-import { Board } from "./board.model";
+import { Board, BoardStatus } from "./board.model";
 import { CreateBoardDto } from "./dto/create-board.dto";
 
 @Controller('boards')
@@ -17,5 +17,29 @@ export class BoardsController {
 		@Body() createBoardDto: CreateBoardDto
 	): Board {
 		return this.boardsService.createBoard(createBoardDto);
+	}
+
+	@Get('/:id')
+	getBoardById(@Param('id') id: string): Board {
+		return this.boardsService.getBoardById(id);
+	}
+
+	@Delete('/:id')
+	deleteBoard(@Param('id') id: string): void {
+		this.boardsService.deleteBoard(id);
+	}
+
+	/*
+	데코레이터
+	@Param: URL 경로에서 파라미터 값을 추출
+	@Body: 
+	*/
+
+	@Patch('/:id/status')
+	updateBoardStatus(
+		@Param('id') id: string,
+		@Body('status') status: BoardStatus
+	) {
+		return this.boardsService.updateBoardStatus(id, status);
 	}
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -13,7 +13,7 @@ export class BoardsService {
 
 	createBoard(createBoardDto: CreateBoardDto) {
 		const { title, description } = createBoardDto;
-		
+
 		const board: Board = {
 			id: uuid(),
 			title,
@@ -22,6 +22,20 @@ export class BoardsService {
 		}
 
 		this.boards.push(board);
+		return board;
+	}
+
+	getBoardById(id: string): Board {
+		return this.boards.find((board) => board.id === id);
+	}
+
+	deleteBoard(id: string): void {
+		this.boards = this.boards.filter((board) => board.id !== id);
+	}
+
+	updateBoardStatus(id: string, status: BoardStatus): Board {
+		const board = this.getBoardById(id);
+		board.status = status;
 		return board;
 	}
 }
