0e10234bec0f8cd95ac404d72393b73e8d72d482
jungeun919 <renee8369@likelion.org>
2023-10-06 13:56:55 +0900

    create jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
 		TypeOrmModule.forFeature([User]),
 	],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,31 @@ import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
 import { UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 export class AuthService {
 	constructor(
 		@InjectRepository(UserRepository)
-		private userRepository: UserRepository
+		private userRepository: UserRepository,
+		private jwtService: JwtService
 	) {}
 
 	async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
 		return this.userRepository.createUser(authCredentialDto);
 	}
 
-	async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+	async signIn(authCredentialDto: AuthCredentialDto): Promise<{accessToken: string}> {
 		const { username, password } = authCredentialDto;
 		// const user = await this.userRepository.findOne({ username });
 		const user = await this.userRepository.findOne({ where: {username} });
 
 
 		if (user && (await bcrypt.compare(password, user.password))) {
-			return 'login success';
+			// 유저 토큰 생성 (secret + payload)
+			const payload = { username };
+			const accessToken = await this.jwtService.sign(payload);
+
+			return { accessToken };
 		} else {
 			throw new UnauthorizedException('login failed');
 		}
